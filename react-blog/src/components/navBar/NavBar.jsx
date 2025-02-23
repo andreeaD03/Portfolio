217eded9115736cd52c69f069e2e49f0df589c74
unknown <andreeadan0302@gmail.com>
2025-02-23 23:06:18 +0200

    nav in progres

---
+++ react-blog/src/components/navBar/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./NavBar.css";
+
+const NavBar = () => {
+  return (
+    <div className="nav">
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+      </ul>
+    </div>
+  );
+};
+
+export default NavBar;

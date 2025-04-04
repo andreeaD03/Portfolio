01aee1ae56f22f1cdd3fc7c6508e30b37ea2d578
unknown <andreeadan0302@gmail.com>
2025-04-04 22:35:30 +0300

    stashed changes

---
+++ react-blog/src/navBar/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./NavBar.css";
+
+const NavBar = () => {
+  return (
+    <div className="nav">
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Posts</li>
+      </ul>
+    </div>
+  );
+};
+
+export default NavBar;
